a41873ae7b4b1b6f25af75166c4005d364c988f3
mojtaba <mojtaba_kh@hotmail.com>
2022-04-24 10:58:36 +0430

    Added Project To Src

---
+++ src/FizzBuzz/BuzzFizz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class BuzzFizz
+    {
+        public string FBGame()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    return "fizz";
+                }
+
+                else if (i % 5 == 0)
+                {
+                    return "buzz";
+                }
+
+                else if (i % 5 == 0 && i % 3 == 0)
+                {
+                    return "FizzBuzz";
+                }
+            }
+            return "Invali";
+        }
+    }
+}
